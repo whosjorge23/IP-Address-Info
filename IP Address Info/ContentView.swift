72221423eafb033265217af7eb490f41610c519d
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-25 12:33:21 +0100

    Initial Commit

---
+++ IP Address Info/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IP Address Info
+//
+//  Created by Giorgio Giannotta on 25/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IP Address Info/IP_Address_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IP_Address_InfoApp.swift
+//  IP Address Info
+//
+//  Created by Giorgio Giannotta on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct IP_Address_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
